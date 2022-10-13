6548622174be0340cfe6b82d4afa4db8122bbe23
Sun-ning1 <chinaisrising999@gmail.com>
2022-10-13 10:28:23 -0600

    update

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+from urllib.parse import urlparse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,7 +34,15 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        hostname = urllib.parse.urlparse(url).hostname
+        port = urllib.parse.urlparse(url).port
+        if port == None:
+            port = 80
+        path = urllib.parse.urlparse(url).path
+        if path == '':
+            path = "/"
+        return hostname, port, path
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +50,20 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        data_list = data.split(' ')
+        str_code = data_list[1]
+        int_code = int(str_code)
+        return int_code
 
     def get_headers(self,data):
-        return None
+        data_list = data.split('\r\n\r\n')
+        headers = data_list[0]
+        return headers
 
     def get_body(self, data):
-        return None
+        data_list = data.split("\r\n\r\n")
+        body = data_list[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +84,47 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        host,port,path = self.get_host_port(url)
+        self.connect(host,port)
+        agent =  "User-Agent: curl/7.64.1\r\n"
+        accept = "Accept: */*\r\n"
+        status_code = "GET {} HTTP/1.1\r\n".format(path)
+        host_n = "Host: {}\r\n".format(host)
+        connection = "Connection: close\r\n\r\n"
+        payload = status_code + agent + host_n + accept + connection
+        self.sendall(payload)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        host,port,path = self.get_host_port(url)
+        self.connect(host, port)
+        agent =  "User-Agent: curl/7.64.1\r\n"
+        content_type="Content-Type: application/x-www-form-urlencoded\r\n"
+        status_code = "POST {} HTTP/1.1\r\n".format(path)
+        host_n = "Host: {}\r\n".format(host)
+        connection = "Connection: close\r\n\r\n"
+        content_type="Content-Type: application/x-www-form-urlencoded\r\n"
+        if args != None:
+            parameters = urllib.parse.urlencode(args)
+            content_length = "Content-Length: {}\r\n".format(str(len(parameters)))
+        else:
+            parameters = ''
+            content_length="Content-Length:{}\r\n".format(str(0))
+            
+        payload = status_code + agent + host_n + content_type + content_length + connection + parameters
+        self.sendall(payload)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
+
+
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
